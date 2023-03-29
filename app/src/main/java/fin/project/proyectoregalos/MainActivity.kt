cf907c5407afd4dd96b7f22131b676e3d8e62f6c
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2023-03-28 17:50:16 -0700

    Primer commit

---
+++ app/src/main/java/fin/project/proyectoregalos/MainActivity.kt
@@ -0,0 +1,11 @@
+package fin.project.proyectoregalos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
